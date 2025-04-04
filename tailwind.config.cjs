0ef41d3cd62910deaec9db4dff58516a82e09d81
ehab pc <ehabwalied262@gmail.com>
2025-04-04 23:23:15 +0200

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#d00000",
+          darkBackground: "#0D0D0D",
+          blank: "#f1f0ea",
+          secondaryBlank: "#d0cfcf",
+          transparent: 'transparent',
+          coolGrey: "#bfc9ca",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
